9038f830d6a1b424e8938b823aee49b087dcf85c
梁柱 <ymblender@gmail.com>
2020-07-26 23:00:09 +0800

    fix: change lint

---
+++ umd/newFn.d.ts
@@ -0,0 +1,3 @@
+export declare function newFnReturn(code: string): any;
+export declare function newFnRun(code: string): any;
+export declare function runFn(fn: any, ...args: any[]): any;
---
+++ umd/ob.d.ts
@@ -0,0 +1,3 @@
+export declare const watch: Set<unknown>;
+export declare const events: Map<any, any>;
+export declare function dispatch(): void;
